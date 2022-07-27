688dd62bed8527b0eb8905e10dfbf995831abaf5
shiluanzzz <45506006+shiluanzzz@users.noreply.github.com>
2022-07-27 08:50:55 +0800

    0727-54

--- leetcode/editor/cn/54.py
+++ leetcode/editor/cn/54.py
@@ -3,7 +3,7 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
-    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+    def spiralOrder2(self, matrix: list[list[int]]) -> list[int]:
         l, r, t, b = -1, len(matrix[0]), -1, len(matrix)
         i, j = 0, 0
         ans = []
@@ -31,10 +31,34 @@ class Solution:
             if len(ans) == target_lens: break
         return ans
 
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        top, right, down, left = 0, len(matrix[0]) - 1, len(matrix) - 1, 0
+        ans = []
+        while top <= down and right >= left:
+            for i in range(left, right + 1):
+                ans.append(matrix[top][i])
+            top += 1
+            for i in range(top, down + 1):
+                ans.append(matrix[i][right])
+            right -= 1
+            for i in range(right, left - 1, -1):
+                ans.append(matrix[down][i])
+            down -= 1
+            for i in range(down, top - 1, -1):
+                ans.append(matrix[i][left])
+            left += 1
+        lens = len(matrix) * len(matrix[0])
+        return ans[:lens]
+
 
 # leetcode submit region end(Prohibit modification and deletion)
 
 if __name__ == '__main__':
-    print(Solution().spiralOrder([
-        [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]
-    ]))
+    params = """
+    [[6,7,9]]
+    [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+    [[1,2,3],[4,5,6],[7,8,9]]
+    """
+    from leetcode import tools
+
+    tools.test_func_batch(Solution().spiralOrder, params)
---
+++ leetcode/tools/__init__.py
@@ -0,0 +1,1 @@
+from .tools import test_func_batch
